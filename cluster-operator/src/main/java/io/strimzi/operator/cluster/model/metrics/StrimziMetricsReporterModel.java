f06f021110028f37c4325d112b3ee4fa567bc8cd
ocorriga <ocorriga@redhat.com>
2024-12-05 11:28:49 +0000

    Latest changes from Main

---
+++ cluster-operator/src/main/java/io/strimzi/operator/cluster/model/metrics/StrimziMetricsReporterModel.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright Strimzi authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+package io.strimzi.operator.cluster.model.metrics;
+
+import io.strimzi.api.kafka.model.common.HasConfigurableMetrics;
+import io.strimzi.api.kafka.model.common.metrics.StrimziReporterMetrics;
+import io.strimzi.kafka.oauth.common.ConfigException;
+import io.strimzi.operator.common.model.InvalidResourceException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+/**
+ * Represents a model for components with configurable metrics using Strimzi Reporter
+ */
+public class StrimziMetricsReporterModel {
+
+    /**
+     * Name of the Strimzi metrics port
+     */
+    public static final String METRICS_PORT_NAME = "http-prometheus";
+
+    /**
+     * Number of the Strimzi metrics port
+     */
+    public static final int METRICS_PORT = 9404;
+    private final boolean isEnabled;
+    private final List<String> allowList;
+
+    /**
+     * Constructs the StrimziMetricsReporterModel for managing configurable metrics with Strimzi Reporter
+     *
+     * @param specSection StrimziReporterMetrics object containing the metrics configuration
+     */
+    public StrimziMetricsReporterModel(HasConfigurableMetrics specSection) {
+        if (specSection.getMetricsConfig() != null) {
+            if (specSection.getMetricsConfig() instanceof StrimziReporterMetrics spec) {
+                validate(spec);
+                this.isEnabled = true;
+                this.allowList = spec.getValues() != null &&
+                        spec.getValues().getAllowList() != null
+                        ? spec.getValues().getAllowList() : null;
+            } else {
+                throw new ConfigException("Unsupported metrics type " + specSection.getMetricsConfig().getType());
+            }
+        } else {
+            this.isEnabled = false;
+            this.allowList = null;
+        }
+    }
+
+    /**
+     * @return True if metrics are enabled. False otherwise.
+     */
+    public boolean isEnabled() {
+        return isEnabled;
+    }
+
+    /**
+     * Validates the Strimzi Reporter Metrics configuration
+     *
+     * @param spec StrimziReporterMetrics configuration to validate
+     */
+    /* test */  static void validate(StrimziReporterMetrics spec) {
+        List<String> errors = new ArrayList<>();
+        if (spec.getValues() != null && spec.getValues().getAllowList() != null) {
+            if (spec.getValues().getAllowList().isEmpty()) {
+                errors.add("Allowlist should contain at least one element");
+            }
+            for (String regex : spec.getValues().getAllowList()) {
+                try {
+                    Pattern.compile(regex);
+                }       catch (PatternSyntaxException e) {
+                    errors.add(String.format("Invalid regex: %s, %s", regex, e.getDescription()));
+                }
+            }
+        }
+        if (!errors.isEmpty()) {
+            throw new InvalidResourceException("Metrics configuration is invalid: " + errors);
+        }
+    }
+
+    /**
+     * Returns the allowlist as a comma-separated string wrapped in an Optional.
+     *
+     * @return an Optional containing the comma-separated allowlist if it is not null, otherwise an empty Optional
+     */
+    public Optional<String> getAllowList() {
+        return allowList != null ? Optional.of(String.join(",", allowList)) : Optional.empty();
+    }
+}
---
+++ cluster-operator/src/test/java/io/strimzi/operator/cluster/model/metrics/StrimziMetricsReporterModelTest.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright Strimzi authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+package io.strimzi.operator.cluster.model.metrics;
+
+import io.strimzi.api.kafka.model.common.metrics.StrimziReporterMetrics;
+import io.strimzi.api.kafka.model.common.metrics.StrimziReporterMetricsBuilder;
+import io.strimzi.api.kafka.model.common.metrics.StrimziReporterValues;
+import io.strimzi.api.kafka.model.connect.KafkaConnectSpecBuilder;
+import io.strimzi.api.kafka.model.kafka.KafkaClusterSpecBuilder;
+import io.strimzi.operator.common.model.InvalidResourceException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class StrimziMetricsReporterModelTest {
+
+    @Test
+    public void testDisabled() {
+        StrimziMetricsReporterModel metrics = new StrimziMetricsReporterModel(new KafkaConnectSpecBuilder().build());
+
+        assertThat(metrics.isEnabled(), is(false));
+        assertThat(metrics.getAllowList(), is(Optional.empty()));
+        assertTrue(metrics.getAllowList().isEmpty());
+    }
+
+    @Test
+    public void testStrimziMetrics() {
+        StrimziReporterMetrics metricsConfig = new StrimziReporterMetricsBuilder()
+                .withNewValues()
+                    .withAllowList(List.of("kafka_log.*", "kafka_network.*"))
+                .endValues()
+                .build();
+        StrimziMetricsReporterModel metrics = new StrimziMetricsReporterModel(new KafkaClusterSpecBuilder()
+                .withMetricsConfig(metricsConfig).build());
+
+        assertThat(metrics.isEnabled(), is(true));
+        assertTrue(metrics.getAllowList().isPresent());
+        assertThat(metrics.getAllowList().get(), is("kafka_log.*,kafka_network.*"));
+    }
+
+    @Test
+    public void testValidAllowlist() {
+        StrimziReporterValues values = new StrimziReporterValues();
+        values.setAllowlist(List.of("kafka_log.*", "kafka_network.*"));
+
+        List<String> allowlist = values.getAllowList();
+        Assertions.assertFalse(allowlist.isEmpty());
+        Assertions.assertEquals("metric1.*", allowlist.get(0));
+        Assertions.assertEquals("metric2.*", allowlist.get(1));
+    }
+
+    @Test
+    public void testValidation() {
+        // if I pass valid list, doesn't throw error
+        Assertions.assertDoesNotThrow(() -> StrimziMetricsReporterModel.validate(new StrimziReporterMetricsBuilder()
+                        .withNewValues()
+                   // .withAllowList(List.of("kafka_log.*", "kafka_network.*"))
+                .endValues()
+                .build())
+        );
+
+        InvalidResourceException ise0 = Assertions.assertThrows(InvalidResourceException.class, () -> StrimziMetricsReporterModel.validate(
+                new StrimziReporterMetricsBuilder()
+                .withNewValues()
+                //    .withAllowList(List.of())
+                .endValues()
+                .build())
+        );
+        assertThat(ise0.getMessage(), is("Metrics configuration is invalid: [Allowlist should contain at least one element]"));
+
+
+        InvalidResourceException ise1 = Assertions.assertThrows(InvalidResourceException.class, () -> StrimziMetricsReporterModel.validate(
+                new StrimziReporterMetricsBuilder()
+                        .withNewValues()
+               //         .withAllowList(List.of("kafka_network.*", "kafka_log.***", "[a+"))
+                        .endValues()
+                        .build())
+        );
+        assertThat(ise1.getMessage(), is("Metrics configuration is invalid: [Invalid regex: kafka_log.***, Dangling meta character '*', Invalid regex: [a+, Unclosed character class]"));
+    }
+
+}
